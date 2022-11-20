21c1ebe2c34f6afd76ebae254e6e1b4317adc67d
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-11-20 11:29:32 -0500

    loading page redirect to login

---
+++ src/components/routes/Loading.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import loadingGIF from "../../images/loading.gif"
+
+export default function Loading() {
+    //state
+    const [count, setCount] = useState(3);
+    //hooks
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount((curentCount) => --curentCount);
+        }, 1000)
+        // redirect once count is equal to 0
+        count === 0 && navigate("/login");
+        // cleanup
+        return ()  => clearInterval(interval);
+    }, [count]);
+
+    return (
+        <div className="d-flex justify-content-center align-items-center" 
+             style={{ height: "90vh"}}
+        >
+            <img src={loadingGIF} alt="Loading" style={{width: "400px"}} />
+            </div>
+    )
+
+}
