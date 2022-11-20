a5b50b937899c24e045ca70fcafb89cc8cca4afd
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-11-20 17:53:49 -0500

    added CRUD functionality to categories

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from "./context/auth";
+import { DatePicker } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/admin/Category.js
+++ src/pages/admin/Category.js
@@ -1,15 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../context/auth";
 import Jumbotron from "../../components/cards/Jumbotron";
 import AdminMenu from "../../components/nav/AdminMenu";
 import axios from "axios";
 import toast from "react-hot-toast";
+import CategoryForm from "../../components/forms/CategoryForm";
+import {Modal} from "antd";
 
 export default function AdminCategory () {
     //context
     const [auth, setAuth] = useAuth();
     // state
     const [name, setName] = useState("");
+    const [categories, setCategories] = useState([]);
+    const [visible, setVisible] = useState(false);
+    const [selected, setSelected] = useState(null);
+    const [updatingName, setUpdatingName] = useState("");
+
+    useEffect(() => {
+      loadCategories();
+    }, []);
+
+    const loadCategories = async () => {
+      try {
+        const { data } = await axios.get("/categories");
+        setCategories(data);
+      } catch (err) {
+        console.log(err)
+      }
+    }
 
     const handleSubmit = async (e) => {
       e.preventDefault();
@@ -18,12 +37,53 @@ export default function AdminCategory () {
         if(data?.error) {
           toast.error(data.error)
         } else {
+          loadCategories();
           setName("");
           toast.success(`"${data.name}" is created`)
         }
       } catch (err) {
         console.log(err)
         toast.error("Create category failed. Try again.")
+      }
+    };
+
+    const handleUpdate = async (e) => {
+      e.preventDefault();
+      try {
+        const {data} = await axios.put(`/category/${selected._id}`, {
+          name: updatingName,
+        });
+        if(data?.error) {
+          toast.error(data.error);
+        } else {
+          toast.success(`${data.name} is updated`);
+          setSelected(null);
+          setUpdatingName("");
+          loadCategories();
+          setVisible(false);
+        }
+      } catch (err) {
+        console.log(err);
+        toast.error("category may already exist. Try again")
+      }
+    };
+
+    const handleDelete = async (e) => {
+      e.preventDefault();
+      try {
+        const {data} = await axios.delete(`/category/${selected._id}`);
+        if(data?.error) {
+          toast.error(data.error);
+        } else {
+          toast.success(`${data.name} has been deleted`);
+          setSelected(null);
+          setUpdatingName("");
+          loadCategories();
+          setVisible(false);
+        }
+      } catch (err) {
+        console.log(err);
+        toast.error("category may already exist. Try again")
       }
     };
 
@@ -42,18 +102,44 @@ export default function AdminCategory () {
             <div className="col-md-9">
               <div className="p-3 mt-2 mb-2 h4 bg-light">Manage Categories</div> 
 
-              <div className="p-3">
-                <form onSubmit = {handleSubmit}>
-                  <input 
-                    type="text" 
-                    className="form-control p-3"
-                    placeholder="Category name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                  <button className="btn btn-primary mt-3">Submit</button>
-                </form>
-              </div>
+              <CategoryForm 
+                value={name}
+                setValue={setName}
+                handleSubmit={handleSubmit}
+              />
+
+              <hr />
+
+               <div className="col">
+                  {categories?.map((c) => (                 
+                    <button 
+                    key={c._id}
+                    className="btn btn-outline-primary m-3" 
+                    onClick={() => {
+                      setVisible(true);
+                      setSelected(c);
+                      setUpdatingName(c.name);
+                    }}>
+                      {c.name}
+                    </button>
+                    ))}
+                </div>
+
+                <Modal 
+                  visible={visible} 
+                  onOk={() => setVisible(false)} 
+                  onCancel={() => setVisible(false)} 
+                  footer={null}
+                >
+                
+                <CategoryForm 
+                  value={updatingName}
+                  setValue={setUpdatingName}
+                  handleSubmit={handleUpdate}
+                  buttonText="Update"
+                  handleDelete={handleDelete}
+                />           
+                </Modal>                                        
             </div>
           </div>
         </div>
